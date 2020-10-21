76b8dd4d5860c18253ffdf52ed8b599d29da1f0b
Dylan <dylan.albert.osborne@gmail.com>
2020-10-21 15:10:33 +0100

    Reworked the Game!
    Replaced the grid system with individual prefabs, ui is now working, as well as score tracking and bee production.

---
+++ Project/Assets/Scripts/CombBehavior.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombBehavior : MonoBehaviour
+{
+    enum Capacity { EMPTY, LOW, MEDIUM, HIGH }
+
+    public Sprite[] sprites;
+
+    Capacity myCapacity;
+    PlayerController playerController;
+
+    float timer;
+    float interval;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCapacity = Capacity.HIGH;
+        RefreshSprite();
+        playerController = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > (1/Mathf.Max(playerController.bees, 1))*5)
+        {
+            timer = Random.Range(0,3);
+            if ((int)myCapacity < 3)
+                myCapacity = myCapacity+1;
+            RefreshSprite();
+        }
+    }
+
+    public int OnClick()
+    {
+        int honeyToReturn = 1;
+        switch (myCapacity)
+        {
+            case Capacity.EMPTY:
+                Debug.Log("Clicked on Empty Honeycomb!");
+                honeyToReturn = 0;
+                break;
+
+            case Capacity.LOW:
+                myCapacity = Capacity.EMPTY;
+                break;
+
+            case Capacity.MEDIUM:
+                myCapacity = Capacity.LOW;
+                break;
+
+            case Capacity.HIGH:
+                myCapacity = Capacity.MEDIUM;
+                break;
+        }
+        RefreshSprite();
+        return honeyToReturn;
+    }
+
+    void RefreshSprite()
+    {
+        GetComponentInChildren<SpriteRenderer>().sprite = sprites[(int)myCapacity];
+    }
+}
